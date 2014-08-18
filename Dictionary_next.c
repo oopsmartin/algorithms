852689ff3d9a13c4fba7a1a3152f766c23c5d112
oopsmartin <oopsmartin@163.com>
2014-08-18 10:31:54 +0800

    ..

--- Dictionary_next.c
+++ Dictionary_next.c
@@ -1,36 +1,64 @@
 #include <stdio.h>
 
-int Bitswap(char* a, int n);
-void Bitsort(char* a, int n);
+int Bitswap(int* a, int n);
+void Bitsort(int* a, int n);
 void Qsort(int start,int length,int a[]);
 
 // 1. the n bit number is considered as a char *
-int Bitswap(char* a, int n)
+int Bitswap(int* a, int n)
 {
 	int i;
 	int j;
 	int cursor;
+	int minCursor = -1;
+	int min;// the number which is minimum among the digits which are larger than the digit 
+	
 	for(i=n-2;i>=0;i--){
+		printf("a[i] is %d\n",a[i]);	
+		min = a[i];
+		cursor = i;
+
 		for(j=i+1;j<n;j++){
 			if(a[i]<a[j]){
-				cursor = i;
-				i = -1;
-				break;
-			}
+				if(min==a[i]){
+					printf("yeah\n");
+					min = a[j];
+					cursor = i;
+					minCursor = j;
+					printf("min is %d\n",min);
+				}	
+				if(a[j]<min){
+					min = a[j];
+					cursor = i;
+					minCursor = j;
+					printf("now min is %d\n",min);
+				}
+			}			
+		}// for j
+		if(min!=a[i]){
+			printf("cursor is %d\n",cursor);
+			break;
 		}
-	}	
-	char tmp = a[cursor];
-	a[cursor] = a[j];
-	a[j] = tmp;	
-	return i+1;
+	}//for i
+		
+	int tmp;
+	tmp = a[cursor];
+	a[cursor] = a[minCursor];
+	a[minCursor] = tmp;
+	
+	for(j=0;j<n;j++)
+		printf("%d ",a[j]);
+	printf("\n");
+	return cursor+1;
 }
 
 // 2. the rest digits should be sorted from small to big
-void Bitsort(char* a, int n)
+void Bitsort(int* a, int n)
 {
-	int i = Bitswap(a, n);	
+	int i = Bitswap(a, n);
+	printf("now i is %d\n",i);
 	for(;i<n;i++){
-		Qsort(i,3,a);
+		Qsort(i,n,a);
 	}
 }
 
@@ -69,8 +97,14 @@ void Qsort(int start,int length,int a[])
 
 int main()
 {
-	char a[] = {'2','1','3'};	
-	Bitsort(a,3);
+	int i;
+	int a[] = {2,3,5,4,1};
+	int length = sizeof(a)/sizeof(int);
+	printf("length is %d\n",length);
+	Bitsort(a,5);
+	for(i=0;i<length;i++)
+		printf("%d ",a[i]);
+	//printf("\n");
 	getchar();
 	getchar();
 	return 0;
